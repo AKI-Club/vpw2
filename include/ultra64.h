c00f5d6a1fd69f2153bbb20ebf59559045af8ea4
gumorr <gumorr@gummiwelt.net>
2023-06-02 18:47:49 -0500

    actually makes the changes i need

--- include/ultra64.h
+++ include/ultra64.h
@@ -1,6 +1,10 @@
 #ifndef _ULTRA64_H_
 #define _ULTRA64_H_
 
+// should probably add these as compiler flags
+#define _LANGUAGE_C 1
+#define _MIPS_SZLONG 32
+
 #include <PR/ultratypes.h>
 /*
 #include <PR/rcp.h>
