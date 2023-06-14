5e3ba52413ba2fe35589fa5f3633e70b18031b8b
freem <aj187@ajworld.net>
2023-06-14 18:51:09 -0500

    add C and linker placeholders for segments 1 and 2

---
+++ src/seg1_cutscene/text_seg1.c
@@ -0,0 +1,7 @@
+/* Virtual Pro-Wrestling 2 decompilation project - Segment 1 main C file
+ * Segment 1 is used for "cutscenes" (i.e. the game intro and ending,
+ * wrestler entrances, unlock notifications, celebration scenes).
+ */
+/*============================================================================*/
+#include "common.h"
+#include "ultra64.h"
---
+++ src/seg2_menu2/text_seg2.c
@@ -0,0 +1,6 @@
+/* Virtual Pro-Wrestling 2 decompilation project - Segment 2 main C file
+ * Segment 2 is the second of two menu-dedicated segments.
+ */
+/*============================================================================*/
+#include "common.h"
+#include "ultra64.h"
