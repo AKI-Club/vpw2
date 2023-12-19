ccc299f214b526a41adf78b1fe13804d5aed16ce
freem <aj187@ajworld.net>
2023-12-18 18:48:58 -0600

    add akifiletable to tools external repo available at https://github.com/AKI-Club/akifiletable

---
+++ tools/akifiletable.h
@@ -0,0 +1,28 @@
+#ifndef AKIFILETABLE_H_
+#define AKIFILETABLE_H_
+
+typedef struct {
+	char *listFilename; /* input list */
+	char *outDataFilename; /* output filetable data */
+	char *outIndexFilename; /* output filetable index */
+	char *outHeaderFilename; /* output header filename */
+	bool verbose;
+} InputArgs;
+
+static const InputArgs defaultArgs = {
+	.listFilename = NULL,
+	.outDataFilename = "filetable.bin",
+	.outIndexFilename = "filetable.idx",
+	.outHeaderFilename = "filetable.h",
+	.verbose = false
+};
+
+typedef struct {
+	char *file;        /* path to file to include */
+	bool lzss;         /* if true, mark file as LZSS encoded in the index */
+	char *symbol;      /* optional export symbol in the format "FILEID_%s" */
+	bool exportsize;   /* if true, export a symbol with the filesize. symbol must not be NULL */
+	int exportsizepad; /* optional; pad output filesize symbol value; only used if exportsize==true */
+} ListEntry;
+
+#endif
